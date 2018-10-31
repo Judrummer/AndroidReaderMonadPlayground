36f4f025dbe05aa8144455f2d3969a747f33a95b
Ju Tipatai <tipatai.puthanukunkit@agoda.com>
2018-10-31 16:59:30 +0700

    data layer

--- app/src/main/java/com/judrummer/androidreadermonadplayground/data/AppPreference.kt
+++ app/src/main/java/com/judrummer/androidreadermonadplayground/data/AppPreference.kt
@@ -1,2 +1,13 @@
 package com.judrummer.androidreadermonadplayground.data
 
+import com.judrummer.androidreadermonadplayground.delegate.PaperDelegate
+
+interface AppPreference {
+    var userProfileEntity: UserProfileEntity?
+    var userToken: String?
+}
+
+class AppPreferenceImpl : AppPreference {
+    override var userProfileEntity: UserProfileEntity? by PaperDelegate()
+    override var userToken: String? by PaperDelegate()
+}
